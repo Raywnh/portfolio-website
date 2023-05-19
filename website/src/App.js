cbe67077ab2a3ee5acbcf86e87f7f1a813a365cd
Ray Ho <108425957+Raywnh@users.noreply.github.com>
2023-05-19 16:09:26 -0700

    Update App.js

--- website/src/App.js
+++ website/src/App.js
@@ -30,7 +30,7 @@ function App() {
         </ul>
       </div>
       <div className="home">
-        <div className='name'>Hello, i am</div>
+        <div className='name'>Hello world, i am</div>
         <h2 className='ray'>RAY HO</h2>
         <div className="typed">
           <Typewriter
@@ -47,11 +47,46 @@ function App() {
       </div>
       <div className='about'>
         <div className='image-placeholder'>
-          
+
         </div>
         <div className='text'>
-          <h1>PLACEHOLDER</h1>
+          <h1>ABOUT</h1>
+          <div className="line">__________________________________________</div>
+          <h2>Hey there! My name is Ray Ho, I'm a third year Computer Engineering student studying at the University of British Columbia. So far, I've had the pleasure
+            of working as a software developer with a <a href="https://www.cityu.edu.hk/">university based in Hong Kong</a>, and a <a href="https://github.com/BEARUBC">student-led engineering design team from UBC</a>.
+          </h2>
+          <h2 className="second">I'm currently a software engineer intern at <a href="https://www.alida.com/">Alida</a>.
+          I'm open to any tech internship roles starting in Spring 2024 and Summer 2024!
+          </h2>
+          <div className='technologies'>
+            <h2>Technologies</h2>
+            <div className="list-container">
+              <ul>
+                <li>Stuff</li>
+                <li>Stuff</li>
+                <li>Stuff</li>
+                <li>Stuff</li>
+                <li>Stuff</li>
+                <li>Stuff</li>
+                <li>Stuff</li>
+                <li>Stuff</li>
+              </ul>
+              <ul>
+                <li>Stuff</li>
+                <li>Stuff</li>
+                <li>Stuff</li>
+                <li>Stuff</li>
+                <li>Stuff</li>
+                <li>Stuff</li>
+                <li>Stuff</li>
+                <li>Stuff</li>
+              </ul>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className='experience'>
+
       </div>
     </div>
   );
